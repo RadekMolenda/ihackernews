f834b000911be64b528c1e59a1cfdd6821047a80
Radek Molenda <radek.molenda@gmail.com>
2013-02-01 23:01:16 +0100

    Introduce median method

--- lib/sample.rb
+++ lib/sample.rb
@@ -5,9 +5,27 @@ class Sample
   end
 
   def mean
-    @ary.reduce(&:+).to_f / @ary.length
+    @ary.reduce(&:+).to_f / len
   end
 
+  def median
+    if len.even?
+      mid = len / 2
+      sorted[(mid-1)..mid].reduce(&:+).to_f / 2
+    else
+      mid = (len - 1) / 2
+      sorted[mid]
+    end
+  end
+
+  private
+  def sorted
+    @ary.sort
+  end
+
+  def len
+    @ary.length
+  end
 end
 
 class SampleSizeIsZero < Exception
--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -15,4 +15,15 @@ describe Sample do
     end
   end
 
+  describe "#median" do
+    it "should be correct" do
+      expect(Sample.new([1]).median).to eq 1
+      expect(Sample.new([1, 2]).median).to eq 1.5
+      expect(Sample.new([0]).median).to eq 0
+      expect(Sample.new([-1, 1, -3]).median).to eq -1.0
+      expect(Sample.new([-5, -1, 1, -3]).median).to eq -2
+      expect(Sample.new([0, -1, 1, -3]).median).to eq -0.5
+    end
+  end
+
 end
