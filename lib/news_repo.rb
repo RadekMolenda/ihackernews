e5f883437172fb58b0d7ee9d4720913e43f33c61
Radek Molenda <radek.molenda@gmail.com>
2013-02-02 00:35:19 +0100

    Introduce NewsRepo class for getting the news with score above median

---
+++ lib/news_repo.rb
@@ -0,0 +1,20 @@
+require_relative './sample'
+
+class NewsRepo
+  extend Forwardable
+
+  def_delegators :sample, :median, :mode, :mean
+
+  def initialize(news)
+    @news = news
+  end
+
+  def above_median
+    @news.select{ |post| post[:points] > median }
+  end
+
+  private
+  def sample
+    @sample ||= Sample.new(@news.map{|post| post[:points]})
+  end
+end
