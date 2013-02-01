ddc05101ef25bde834c3f5476bca1f2fc2580bd9
Radek Molenda <radek.molenda@gmail.com>
2013-02-01 23:35:14 +0100

    Handle bimodal cases

--- lib/mode.rb
+++ lib/mode.rb
@@ -1,2 +1,27 @@
-class Mode
+class ModeNotBimodal < Exception
 end
+
+class Mode
+  def initialize(ary)
+    @ary = ary
+  end
+
+  def to_s
+    values.length == 1 ? "#{values[0]}" : "is bimodal - #{values}"
+  end
+
+  private
+
+  def histogram
+    @ary.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+  end
+
+  def max
+    histogram.values.max
+  end
+
+  def values
+    @values ||= histogram.select{ |k, v| v == max }.keys
+  end
+end
+
---
+++ spec/mode_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/mode'
+
+describe Mode do
+  describe "#to_s" do
+    context "when not bimodal" do
+      it "should be one value only" do
+        expect(Mode.new([1,1,2,3]).to_s).to eq "1"
+        expect(Mode.new([1,1,2,2,2,3]).to_s).to eq "2"
+      end
+    end
+    context "when not bimodal" do
+      it "should have more values" do
+        expect(Mode.new([1,1,2,3,3]).to_s).to eq "is bimodal - [1, 3]"
+        expect(Mode.new([1,1,2,2,3]).to_s).to eq "is bimodal - [1, 2]"
+      end
+    end
+  end
+end
