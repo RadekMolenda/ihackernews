a965b850177675c35c28e83e7ab75c0792b552ec
Radek Molenda <radek.molenda@gmail.com>
2013-02-02 22:35:14 +0100

    Minor copy fix in Mode spec

--- spec/mode_spec.rb
+++ spec/mode_spec.rb
@@ -8,7 +8,7 @@ describe Mode do
         expect(Mode.new([1,1,2,2,2,3]).to_s).to eq "2"
       end
     end
-    context "when not bimodal" do
+    context "when bimodal" do
       it "should have more values" do
         expect(Mode.new([1,1,2,3,3]).to_s).to eq "is bimodal - [1, 3]"
         expect(Mode.new([1,1,2,2,3]).to_s).to eq "is bimodal - [1, 2]"
