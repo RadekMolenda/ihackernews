415fb495dff0899395a5dd11cac23c43613c032c
Radek Molenda <radek.molenda@gmail.com>
2013-02-02 20:55:53 +0100

    Fix specs

--- spec/post_formatter_spec.rb
+++ spec/post_formatter_spec.rb
@@ -1,12 +1,12 @@
 require_relative '../lib/post_formatter'
 
 describe PostFormatter do
-  let(:post) { {title: "Fancy Post", url: "http://www.some-where.ie", postedBy: "Joe Doe"}}
+  let(:post) { {title: "Fancy Post", url: "http://www.some-where.ie", postedBy: "Joe Doe", points: 33}}
 
   describe "#to_s" do
     subject { PostFormatter.new(post) }
     it "formats post" do
-      expect(subject.to_s).to eql "**********\nTitle: Fancy Post\nLink: http://www.some-where.ie\nAuthor: Joe Doe\n\n"
+      expect(subject.to_s).to eql "**********\nTitle: Fancy Post\nLink: http://www.some-where.ie\nPoints: 33\nAuthor: Joe Doe\n\n"
     end
   end
 end
